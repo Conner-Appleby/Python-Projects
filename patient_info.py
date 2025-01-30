714981976090b458333f21bcf944c6ebed5ce82b
Kib-by <connerappleby5@gmail.com>
2025-01-30 12:46:44 -0500

    Add files via upload

---
+++ patient_info.py
@@ -0,0 +1,8 @@
+#Determine patient's name 
+name = input("What is your name? ")
+print("Hello " + name)
+
+#Determine patient's age
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print(age)
